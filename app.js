2ae3f69b5f40864ead4657f536065575dfcf6271
schuylermk <schuyler.m.k@gmail.com>
2016-09-17 15:39:22 -0700

    Modify the addPost function to add a link if user chooses to do so

--- app.js
+++ app.js
@@ -18,7 +18,13 @@ function($scope){
   };
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
-    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0
+    });
     $scope.title = '';
+    $scope.link = '';
   };
+
 }]);
